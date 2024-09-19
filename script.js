e655a9ee363a60131f2bab36c9b0f8f7c0ba917e
Camila Liston <84218260+CAMILALISTON@users.noreply.github.com>
2024-09-18 21:39:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,38 @@ function calcularIDcachorro(){
     document.getElementById('pIdade').innerHTML = ("Seu cachorro não pode se aposentar pois a idade dele é: " + idade)
 
 }
+function CalcularIdade(){
+    let inpIdade = document.getElementById('inpIdade').value
+    let idade = inpIdade
+    if(inpIdade > 17){
+        document.getElementById('pId').innerHTML = ("Voce já é maior de idade, pois tem " + idade);
+    }
+    else
+    document.getElementById('pId').innerHTML = ("Você não é maior de idade, pois tem " + idade);
+}
+
+function executar1008(){
+    let id
+    let horas
+    let valorHora
+    let salario
+    id = Number(prompt("Digite seu ID: "));
+    horas = Number(prompt("Quantas horas você trabalhou?"));
+    valorHora = Number(prompt("Quanto você recebe por hora?"));
+
+    salario = horas * valorHora
+        alert("ID: "+id + "\nSalário: R$"+ salario.toFixed(2))
+}
+
+function executar1009(){
+    let id
+    let salario
+    let totalVendas
+    let comissao
+    id = Number(prompt("Digite seu ID: "));
+    salario = Number(prompt("Qual o seu salário?"));
+    totalVendas = Number(prompt("Qual o valor de vendas que voce realizou?"));
+
+    comissao = (totalVendas*0.15)+salario
+    alert("ID: "+id + "\nSalário R$" +comissao.toFixed(2));
+}
